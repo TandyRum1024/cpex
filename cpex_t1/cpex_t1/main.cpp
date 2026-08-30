6952c224b1707c8402958d0ed1cc546f910caaf5
TandyRum1024 <yubin2892@gmail.com>
2026-08-31 02:02:07 +0900

    cpex_t1: OpenGL library linking + window test
    Do note that this method of using GLFW as dependency seems to be superseded by `FetchContent()` module! let's see if I can make use of it...

--- cpex_t1/cpex_t1/main.cpp
+++ cpex_t1/cpex_t1/main.cpp
@@ -2,9 +2,37 @@
  * CPEX - T1, C++ (re)primer and OpenGL basics
  * ZIK@MMXXVI
  */
-
 #include <iostream>
 
+// OpenGL: GLFW
+#include <GLFW/glfw3.h>
+
 int main() {
-    std::cout << "HELLO WORLD" << std::endl;
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        std::cerr << "[GLFW] GLFW INIT FAILED!" << std::endl;
+        return -1;
+    }
+
+    std::cout << "[GLFW] CREATING WINDOW..." << std::endl;
+    window = glfwCreateWindow(1280, 720, "HELLO WINDOW", NULL, NULL);
+
+    if (!window) {
+        std::cerr << "[GLFW] CREATING WINDOW FAILED!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    std::cout << "[GLFW] TERMINATING!" << std::endl;
+    glfwTerminate();
+    return 0;
 }
